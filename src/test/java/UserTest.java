2d461cc13d880bdb6dc67d5fd63d47d007f16d2f
GilSer <gilyan74@bk.ru>
2024-07-28 01:04:21 +0200

    Initial commit

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+    private User user;
+
+    @BeforeEach
+    public void init() {
+        user = new User("user", "password", "user@email.com");
+    }
+
+    @Test
+    public void getLogin_assertNull_success() {
+        assertNotNull(user.getLogin());
+    }
+
+    @Test
+    public void getPassword_assertFalse_success() {
+        assertFalse(false, user.getPassword());
+        assertTrue(true, user.getPassword());
+    }
+
+    @Test
+    public void getEmail_assertEquals_success() {
+        assertEquals("user@email.com", user.getEmail());
+
+    }
+
+
+    @Test
+    public void setLogin_throwsException_success() {
+        assertThrows(IllegalArgumentException.class, () -> user.setLogin("a"));
+    }
+
+    @Test
+    public void setPassword_throwsException_success() {
+        assertThrows(IllegalArgumentException.class, () -> user.setPassword("abc0125"));
+    }
+
+    @Test
+    public void setEmail_throwsException_success() {
+        assertThrows(IllegalArgumentException.class, () -> user.setEmail("userNewEmail.com"));
+    }
+
+    @Test
+    public void isValidPassword_check_success() {
+        assertTrue(user.isValidPassword("password"));
+        assertFalse(user.isValidPassword("incorrect password"));
+    }
+}
